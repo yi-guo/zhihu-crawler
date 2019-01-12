a844f27649f91487d0fc5e1970e59f1a24320bdf
Yi Guo <yiguocu@gmail.com>
2019-01-11 21:22:41 -0500

    Prefer switch over if-else

--- src/index.js
+++ src/index.js
@@ -12,11 +12,15 @@ const Question = require('./Question');
 function main() {
   const {id, maxAnswers} = args;
   const command = idx(args, _ => _._[0]);
-  if (command === 'question') {
-    new Question(id, maxAnswers).downloadImages();
-  }
-  if (command === 'answer') {
-    new Answer(id).downloadImages();
+  switch (command) {
+    case 'question':
+      new Question(id, maxAnswers).downloadImages();
+      break;
+    case 'answer':
+      new Answer(id).downloadImages();
+      break;
+    default:
+      invariant(false, 'Unrecognized command');
   }
 }
 
